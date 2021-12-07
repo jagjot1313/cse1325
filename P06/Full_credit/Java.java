432bc53ac3ebb6e52e5f2f48332d41d9190bc932
jagjot1313 <89835714+jagjot1313@users.noreply.github.com>
2021-12-07 05:19:51 -0600

    Create Java.java

---
+++ P06/Full_credit/Java.java
@@ -0,0 +1,13 @@
+public class Java 
+{
+  private Darkness Darkness;
+  private shot[] shots;
+
+ public Java(String name, double price, double cost, Darkness darkness)
+ {
+   this.name=name;
+   this.price=price;
+   this.cost=cost;
+   this.darkness=darkness;
+ }
+}
