52e5afb6008467bebfb1ec0984bee4848e149ade
jagjot1313 <89835714+jagjot1313@users.noreply.github.com>
2021-12-07 05:13:13 -0600

    Create Product.java

---
+++ P06/Full_credit/Product.java
@@ -0,0 +1,17 @@
+public class Product 
+{
+  protected String name;
+  protected double price;
+  protected double cost;
+  public Product(String name, double price,double cost)
+  {
+    this.name=name;
+    this.price=price;
+    this.cost=cost;
+  }
+  public String name()
+  {
+    return this.name;
+  }
+  
+}
