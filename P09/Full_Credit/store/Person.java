ea29a30d12f470fe0e164195102e6d04932b87f9
jagjot1313 <89835714+jagjot1313@users.noreply.github.com>
2021-12-07 06:02:42 -0600

    Update Person.java

--- P09/Full_Credit/store/Person.java
+++ P09/Full_Credit/store/Person.java
@@ -1,13 +1,28 @@
-protected void addPerson(Person person) {   this.persons.add(person);   }
+Package Store;
 
-    protected int numberOfPeople() {    return this.persons.size(); }
+protected void addPerson(Person person) 
+{   
+    this.persons.add(person);  
+    this.persons.add(phone);
+}
 
-    protected String personToString(int productIndex) {  return this.products.get(productIndex).name;   }
+    protected int numberOfPeople()
+    {   
+        return this.persons.size();
+    }
+
+    protected String personToString(int productIndex)
+    {  
+        return this.products.get(productIndex).name; 
+        return this.products.get(productIndex).phone;  
+       
+    }
 
     protected String personToString()
     {
         String result = "Welcome to " + storeName + "\n\n";
-        for(int i=0; i<persons.size(); ++i) {
+        for(int i=0; i<persons.size(); ++i) 
+        {
             result += i + ") " + persons.get(i) + "\n";
         }
         return result;
