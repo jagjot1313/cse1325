57e5235291e2665b557f65cf3a7d87a90beb922f
jagjot1313 <89835714+jagjot1313@users.noreply.github.com>
2021-12-07 05:14:15 -0600

    Create Donut.java

---
+++ P06/Full_credit/Donut.java
@@ -0,0 +1,23 @@
+for  Donut 
+public class Donut 
+{
+  private Frosting frosting;
+  private boolean sprinkles;
+  private Filling filling;
+
+  public Donut(String name,double coat ,double price,Frosting frosting, boolean sprinkles, Filling filling)
+  {
+    super(name,coat,price);
+    this.frosting=frosting;
+    this.sprinkles=sprinkles;
+    this.filling=filling;
+  }
+@override
+public String toString()
+{
+  strr="confirmed";
+  return str;
+}
+
+
+}
